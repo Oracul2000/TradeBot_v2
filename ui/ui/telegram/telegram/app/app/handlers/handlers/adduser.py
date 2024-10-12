380e77825e5245256151d6919ce7241d3823a691
Коля <kopytovnn@gmailcom>
2024-10-12 09:34:45 +0300

    [Ui-1] (+) Add architecture & add new user

---
+++ ui/ui/telegram/telegram/app/app/handlers/handlers/adduser.py
@@ -0,0 +1,142 @@
+from aiogram import Dispatcher, types
+from aiogram import Router, F
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, ReplyKeyboardRemove, InlineKeyboardMarkup
+
+from typing import Text
+
+from keyboards import buttons, simple_row
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import Session
+# from database.models import user
+
+
+# engine = create_engine("sqlite:///Data.db", echo=True)
+
+
+router = Router()
+
+coins = ['ADA', 'LINK', 'XRP', 'XLM', 'DASH', 'NEO', 'TRX',
+         'EOS', 'LTC', 'DOGE', 'APT', 'ATOM', 'BTC', 'ETH']
+
+class AddNewUser(StatesGroup):
+    name = State()
+    bybitapi = State()
+    bybitsecret = State()
+    symbol = State()
+    deposit = State()
+
+
+@router.message(Command("add_user"))
+@router.callback_query(F.data == "add_user")
+async def adduser(callback: types.CallbackQuery, state: FSMContext):
+    await callback.message.answer(
+        text="Введите имя пользователя:"
+    )
+    await state.set_state(AddNewUser.name)
+
+
+@router.message(AddNewUser.name)
+async def namechoosen(message: types.Message, state: FSMContext):
+    await state.update_data(name=message.text)
+
+    await state.set_state(AddNewUser.bybitapi.state)
+    await message.answer("Введите API key пользователя от ByBit")
+
+
+@router.message(AddNewUser.bybitapi)
+async def bybitapichoosen(message: types.Message, state: FSMContext):
+    await state.update_data(bybitapi=message.text)
+
+    await state.set_state(AddNewUser.bybitsecret)
+    await message.answer("Введите Secret key пользователя от ByBit")
+
+
+@router.message(AddNewUser.bybitsecret)
+async def bybitsecretchoosen(message: types.Message, state: FSMContext):
+    await state.update_data(bybitsecret=message.text)
+
+    await message.answer("Введите торговую пару", reply_markup=simple_row.make_inline_keyboard(coins))
+    await state.set_state(AddNewUser.symbol.state)
+
+
+@router.message(AddNewUser.symbol)
+@router.callback_query(F.data.startswith("bybit_change_"))
+async def bybitsymbol(callback: types.CallbackQuery, state: FSMContext):
+    await state.update_data(symbol=callback.data.split('_')[2])
+
+    await state.set_state(AddNewUser.deposit.state)
+    await callback.message.answer("Введите желаемый депозит в usdt")
+
+
+async def bybitsymbolclone(callback: types.CallbackQuery, state: FSMContext):
+    await state.set_state(AddNewUser.deposit.state)
+    await callback.message.answer("Введите желаемый депозит в usdt")
+
+
+@router.message(AddNewUser.deposit)
+async def bybitdeposiot(message: types.Message, state: FSMContext):
+    await state.update_data(deposit=message.text.lower())
+    user_data = await state.get_data()
+
+    print(user_data)
+
+    # with Session(engine) as session:
+    #     if 'aid' not in user_data and 'uid' not in user_data:
+    #         print(1)
+    #         nu = user.User(name=user_data["name"])
+    #         na = user.API(name=user_data["name"],
+    #                       bybitapi=user_data["bybitapi"],
+    #                       bybitsecret=user_data["bybitsecret"],
+    #                       symbol=user_data["symbol"],
+    #                       deposit=float(user_data["deposit"]))
+    #         session.add_all([nu, na])
+    #         all_apis = session.query(user.API).all()
+    #         all_users = session.query(user.User).all()
+    #         all_users[-1].apis = [all_apis[-1]]
+    #         await state.update_data(aid=all_apis[-1].id)
+    #         await state.update_data(uid=all_users[-1].id)
+    #     elif 'aid' not in user_data:
+    #         print(2)
+    #         na = user.API(name=user_data["name"],
+    #                       bybitapi=user_data["bybitapi"],
+    #                       bybitsecret=user_data["bybitsecret"],
+    #                       symbol=user_data["symbol"],
+    #                       deposit=float(user_data["deposit"]))
+    #         session.add_all([na])
+    #         all_apis = session.query(user.API).all()
+    #         u = session.query(user.User).filter(
+    #             user.User.id == int(user_data["uid"])).all()[0]
+    #         u.apis.append(all_apis[-1])
+    #         await state.update_data(aid=all_apis[-1].id)
+    #     else:
+    #         print(3)
+    #         a = session.query(user.API).filter(
+    #             user.API.id == int(user_data["aid"])).all()[0]
+
+    #         if "bybitapi" in user_data:
+    #             a.bybitapi = user_data["bybitapi"]
+    #             a.bybitsecret = user_data["bybitsecret"]
+    #         if "symbol" in user_data:
+    #             a.symbol = user_data["symbol"]
+    #         if "deposit" in user_data:
+    #             a.deposit = float(user_data["deposit"])
+
+    #     session.commit()
+
+    #     await message.answer("Данные успешно внесены")
+    #     await message.answer("Подождите...")
+
+    #     from app.handlers.allusers import bybitdeposiotclone
+    #     await bybitdeposiotclone(message, state)
+
+
+# def register_handlers_bybit_auth(dp: Dispatcher):
+#     dp.register_message_handler(adduser, commands="/add_user", state="*")
+#     dp.register_message_handler(namechoosen, state=AddNewUser.name)
+#     dp.register_message_handler(bybitapichoosen, state=AddNewUser.bybitapi)
+#     dp.register_message_handler(
+#         bybitsecretchoosen, state=AddNewUser.bybitsecret)
---
+++ ui/ui/telegram/telegram/app/app/keyboards/keyboards/buttons.py
@@ -0,0 +1,132 @@
+
+from aiogram import Bot, Dispatcher, Router, types
+
+
+EDIT_DATA = types.InlineKeyboardButton(
+    text="Редактировать данные пользователя",
+    callback_data="edit"
+)
+
+ALL_USERS = types.InlineKeyboardButton(
+    text="Пользователи",
+    callback_data="all_users"
+)
+
+ADD_USER = types.InlineKeyboardButton(
+    text="Добавить пользователя",
+    callback_data="add_user"
+)
+
+STARTBYBIT1 = types.InlineKeyboardButton(
+    text="Запустить ByBit",
+    callback_data="bybit_start_"
+)
+
+CHANGE_API = types.InlineKeyboardButton(
+    text="API ключи",
+    callback_data="change_api"
+)
+
+CHANGE_DEPOSIT = types.InlineKeyboardButton(
+    text="Торговый депозит",
+    callback_data="change_deposit"
+)
+
+STATISTIS = types.InlineKeyboardButton(
+    text="Выгрузка статистики",
+    callback_data="get_stat"
+)
+
+# def ADD_TRAIDINGPAIR(uid):
+#     return types.InlineKeyboardButton(
+#         text="Добавить новую пару",
+#         callback_data=f"addtraiding_pairs_{uid}"
+#     )
+
+# def ACTIVE_PAIRS(uid):
+#     return types.InlineKeyboardButton(
+#         text="Активные пары",
+#         callback_data=f"active_pairs_{uid}"
+#     )
+
+# def TRAIDING_PAIRS(uid):
+#     return types.InlineKeyboardButton(
+#         text="Торговые пары",
+#         callback_data=f"traiding_pairs_{uid}"
+#     )
+
+# def STARTBYBIT(uid):
+#     return types.InlineKeyboardButton(
+#         text="Запустить",
+#         callback_data=f"bybit_choosestrat_{uid}"
+#     )
+
+# def CHOOSE(uid):
+#     return types.InlineKeyboardButton(
+#         text="Запустить",
+#         callback_data=f"bybit_start_{uid}"
+#     )
+
+# def STOPBYBIT(uid):
+#     return types.InlineKeyboardButton(
+#         text="Остановить",
+#         callback_data=f"bybit_stop_{uid}"
+#     )
+
+
+# def STOPCLOSEBYBIT(uid):
+#     return types.InlineKeyboardButton(
+#         text="Остановить c закрытием",
+#         callback_data=f"bybit_stopclose_{uid}"
+#     )
+
+
+def COIN(coin):
+    return types.InlineKeyboardButton(
+        text=coin,
+        callback_data=f"bybit_change_{coin}")
+
+# def COIN1(coin):
+#     return types.InlineKeyboardButton(
+#         text=coin,
+#         callback_data=f"bybit_change1_{coin}")
+
+# def COINAPI(api, coin):
+#     print(f"bybit_change2_{coin}_{api}")
+#     return types.InlineKeyboardButton(
+#         text=coin,
+#         callback_data=f"bybit_change2_{coin}${api}")
+
+# def DELETEUSER(uid):
+#     return types.InlineKeyboardButton(
+#         text="Удалить",
+#         callback_data=f"delete_user_{uid}"
+#     )
+
+
+# def DELETEAPI(aid):
+#     return types.InlineKeyboardButton(
+#         text="Удалить",
+#         callback_data=f"delete_api_{aid}"
+#     )
+
+
+def STRATEGY_CONSERVO():
+    return types.InlineKeyboardButton(
+        text="Консерв.(0.2%)",
+        callback_data="strategy_conservo"
+    )
+
+
+def STRATEGY_AGRESSIVE():
+    return types.InlineKeyboardButton(
+        text="Агрес.(0.4%)",
+        callback_data="strategy_agressive"
+    )
+
+
+def STRATEGY_PROF():
+    return types.InlineKeyboardButton(
+        text="Польз.",
+        callback_data="strategy_prof"
+    )
